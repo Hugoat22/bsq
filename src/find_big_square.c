1e0cdcbe4a3fef0b85d473d423f536a67aec1787
Hugo Atlan <hugo.atlan@ens.uvsq.fr>
2025-06-12 16:07:26 +0200

    [ADD] add file test and algo find square

--- src/find_big_square.c
+++ src/find_big_square.c
@@ -14,20 +14,16 @@ int min(int a, int b)
     return (a < b) ? a : b;
 }
 
-int check_square()
-
 int size_square(char **map,int *pos,int size)
 {
     int size_map = my_getnbr(map[0]);
-    int j = -1;
+
+    if (size >= min(size_map - (pos[0] - 1), size_map - pos[1])) return size;
     if (map[pos[0]][pos[1]] == 'o') return 0;
-    for (int i = 0;i != my_power(size,2);i++) {
-        if (i % size == 0) j++;
-        if (map[pos[0] + j][pos[1] + (i % size)] == 'o') {
-            return size;
-        }
-    }
-    if (size == min(size_map - pos[0], size_map - pos[1]) + 1) return size;
+    for (int i = 0;i != size;i++)
+        if (map[pos[0] + size][pos[1] + i] == 'o') return size;
+    for (int j = 0;j != size;j++)
+        if (map[pos[0] + j][pos[1] + size] == 'o') return size;
     return size_square(map,pos,size + 1);
 }
 
